49a0aef2ec79cbc5180a19b20d927e144ff1f614
Vedant Phougat <vedant.phougat@hotmail.com>
2021-08-30 12:25:57 +0530

    Added summary comments

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -5,11 +5,20 @@ namespace WebAPI
 
     public class Program
     {
+        /// <summary>
+        /// Main method, the entry point of the application.
+        /// </summary>
+        /// <param name="args">The args <see cref="string[]"/>.</param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Creates an instance of the host builder.
+        /// </summary>
+        /// <param name="args">The args <see cref="string[]"/>.</param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -8,13 +8,20 @@ namespace WebAPI
 
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// </summary>
+        /// <param name="services">The services <see cref="IServiceCollection"/>.</param>
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The app <see cref="IApplicationBuilder"/>.</param>
+        /// <param name="env">The env <see cref="IWebHostEnvironment"/>.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
