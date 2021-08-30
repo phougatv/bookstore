65cb8b8bd7617775ccb1a68febe092959ddf8bd9
Vedant Phougat <vedant.phougat@hotmail.com>
2021-08-30 12:21:37 +0530

    Removed un-used namespaces

--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -5,10 +5,6 @@ namespace WebAPI
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
 
     public class Startup
     {
