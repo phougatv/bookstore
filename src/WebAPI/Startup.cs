facc2442d56954e0d8f576a52db363c1e8a604cf
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-01 10:27:59 +0530

    Added Swagger.

--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -17,6 +17,10 @@ namespace WebAPI
         {
             services.AddMvc();
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+
             services.AddBookComponent();
         }
 
@@ -31,6 +35,18 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;         // To serve the Swagger UI at the app's root (http://localhost:<port>/).
+            });
 
             app.UseRouting();
 
