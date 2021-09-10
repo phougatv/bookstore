f9d07f1f68125b784e9d1245669091c921fa5fd5
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-10 17:20:43 +0530

    Added exception handling using ASP.NET ExceptionHandling Middleware.

---
+++ src/WebAPI/Errors/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿namespace Atlantis.WebAPI.Errors
+{
+    using Microsoft.AspNetCore.Diagnostics;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using static Microsoft.AspNetCore.Http.StatusCodes;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("on-dev-env")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult ErrorOnDev([FromServices] IWebHostEnvironment webHostEnvironment)
+        {
+            if (webHostEnvironment.EnvironmentName != "Development")
+                throw new InvalidOperationException("This shouldn't be invoked in non-development environments.");
+
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = context.Error;
+            while (exception.InnerException != null) exception = exception.InnerException;
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = exception.Message,
+                Detail = exception.StackTrace,
+            };
+
+            return StatusCode(Status500InternalServerError, problemDetails);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Error() => StatusCode(Status500InternalServerError, new ProblemDetails());
+    }
+}
--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -32,15 +32,19 @@ namespace WebAPI
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/api/error/on-dev-env");
+                //app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/api/error");
             }
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
