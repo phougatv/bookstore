1c3fba886a3f78a8b444b235f43fea33354e111c
Vedant Phougat <vedant.phougat@hotmail.com>
2021-09-13 08:24:56 +0530

    Atlantis.Orders.WebApi: Setup the logging, configuration and web host environment.

--- src/Orders/Atlantis.Orders.WebApi/Program.cs
+++ src/Orders/Atlantis.Orders.WebApi/Program.cs
@@ -5,22 +5,77 @@ namespace Atlantis.Orders.WebApi
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            var configuration = GetConfiguration(environment);
+            var loggerFactory = GetLoggerFactory();
+            var logger = loggerFactory.CreateLogger<Program>();
+            try
+            {
+                logger.LogInformation("Starting the host...");
+                CreateHostBuilder(args, configuration, loggerFactory)
+                    .Build()
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical("Host terminated unexpectedly!");
+#if DEBUG
+                logger.LogError($"Error message: {ex.Message}");
+#endif
+            }
+            finally
+            {
+                logger.LogInformation("Disposing LoggerFactory...");
+                loggerFactory.Dispose();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        /// <summary>
+        /// Creates an instance of the host builder.
+        /// </summary>
+        /// <param name="args">The args <see cref="string[]"/>.</param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(
+            string[] args,
+            IConfiguration configuration,
+            ILoggerFactory loggerFactory)
+        {
+            return Host
+                .CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup(context =>
+                    {
+                        return new Startup(configuration, context.HostingEnvironment, loggerFactory.CreateLogger<Startup>());
+                    });
                 });
+        }
+
+        #region Private Static Methods
+        /// <summary>
+        /// Gets the logger factory <see cref="ILoggerFactory"/>.
+        /// </summary>
+        /// <returns></returns>
+        private static ILoggerFactory GetLoggerFactory() => LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+            builder.AddDebug();
+        });
+
+        /// <summary>
+        /// Gets the configuration <see cref="IConfiguration"/>.
+        /// </summary>
+        /// <param name="environment"></param>
+        /// <returns></returns>
+        private static IConfiguration GetConfiguration(string environment) => new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environment}.json", true, true)
+                .Build();
+        #endregion Private Static Methods
     }
 }
--- src/Orders/Atlantis.Orders.WebApi/Startup.cs
+++ src/Orders/Atlantis.Orders.WebApi/Startup.cs
@@ -3,11 +3,27 @@ namespace Atlantis.Orders.WebApi
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
 
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public ILogger<Startup> Logger { get; }
+
+        public Startup(
+            IConfiguration configuration,
+            IWebHostEnvironment webHostEnvironment,
+            ILogger<Startup> logger)
+        {
+            Configuration = configuration;
+            WebHostEnvironment = webHostEnvironment;
+            Logger = logger;
+        }
+
         /// <summary>
         /// This method gets called by the runtime. Use this method to add services to the container.
         /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
